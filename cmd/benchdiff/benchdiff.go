09571503b01abf970fbb01b445dbcfcdf2d30031
Will Roden <willabides@github.com>
2020-12-29 16:13:47 -0600

    rename to --on-degrade

--- cmd/benchdiff/benchdiff.go
+++ cmd/benchdiff/benchdiff.go
@@ -45,35 +45,35 @@ type benchstatOpts struct {
 }
 
 var benchVars = kong.Vars{
-	"BenchCmdDefault":     `go`,
-	"BenchArgsDefault":    defaultBenchArgsTmpl,
-	"ResultsDirDefault":   filepath.FromSlash("./tmp"),
-	"BenchCountHelp":      `Run each benchmark n times.`,
-	"BenchHelp":           `Run only those benchmarks matching a regular expression.`,
-	"BenchArgsHelp":       `Use these arguments to run benchmarks. It may be a template.`,
-	"PackagesHelp":        `Run benchmarks in these packages.`,
-	"BenchCmdHelp":        `The go command to use for benchmarks.`,
-	"ResultsDirHelp":      `The directory where benchmark output will be deposited.`,
-	"BaseRefHelp":         `The git ref to be used as a baseline.`,
-	"ForceBaseHelp":       `Rerun benchmarks on the base reference even if the output already exists.`,
-	"DegradationExitHelp": `Exit code when there is a degradation in the results.`,
-	"JSONOutputHelp":      `Format output as JSON. When true the --csv and --html flags affect only the "benchstat_output" field.`,
-	"GitCmdHelp":          `The executable to use for git commands.`,
+	"BenchCmdDefault":   `go`,
+	"BenchArgsDefault":  defaultBenchArgsTmpl,
+	"ResultsDirDefault": filepath.FromSlash("./tmp"),
+	"BenchCountHelp":    `Run each benchmark n times.`,
+	"BenchHelp":         `Run only those benchmarks matching a regular expression.`,
+	"BenchArgsHelp":     `Use these arguments to run benchmarks. It may be a template.`,
+	"PackagesHelp":      `Run benchmarks in these packages.`,
+	"BenchCmdHelp":      `The go command to use for benchmarks.`,
+	"ResultsDirHelp":    `The directory where benchmark output will be deposited.`,
+	"BaseRefHelp":       `The git ref to be used as a baseline.`,
+	"ForceBaseHelp":     `Rerun benchmarks on the base reference even if the output already exists.`,
+	"OnDegradeHelp":     `Exit code when there is a statistically significant degradation in the results.`,
+	"JSONOutputHelp":    `Format output as JSON. When true the --csv and --html flags affect only the "benchstat_output" field.`,
+	"GitCmdHelp":        `The executable to use for git commands.`,
 }
 
 var cli struct {
-	BaseRef         string        `kong:"default=HEAD,help=${BaseRefHelp}"`
-	Bench           string        `kong:"default='.',help=${BenchHelp}"`
-	BenchArgs       string        `kong:"default=${BenchArgsDefault},help=${BenchArgsHelp}"`
-	BenchCmd        string        `kong:"default=${BenchCmdDefault},help=${BenchCmdHelp}"`
-	BenchCount      int           `kong:"default=10,help=${BenchCountHelp}"`
-	DegradationExit int           `kong:"name=on-degradation,default=0,help=${DegradationExitHelp}"`
-	ForceBase       bool          `kong:"help=${ForceBaseHelp}"`
-	GitCmd          string        `kong:"default=git,help=${GitCmdHelp}"`
-	JSONOutput      bool          `kong:"help=${JSONOutputHelp}"`
-	Packages        string        `kong:"default='./...',help=${PackagesHelp}"`
-	ResultsDir      string        `kong:"type=dir,default=${ResultsDirDefault},help=${ResultsDirHelp}"`
-	BenchstatOpts   benchstatOpts `kong:"embed"`
+	BaseRef       string        `kong:"default=HEAD,help=${BaseRefHelp}"`
+	Bench         string        `kong:"default='.',help=${BenchHelp}"`
+	BenchArgs     string        `kong:"default=${BenchArgsDefault},help=${BenchArgsHelp}"`
+	BenchCmd      string        `kong:"default=${BenchCmdDefault},help=${BenchCmdHelp}"`
+	BenchCount    int           `kong:"default=10,help=${BenchCountHelp}"`
+	OnDegrade     int           `kong:"name=on-degrade,default=0,help=${OnDegradeHelp}"`
+	ForceBase     bool          `kong:"help=${ForceBaseHelp}"`
+	GitCmd        string        `kong:"default=git,help=${GitCmdHelp}"`
+	JSONOutput    bool          `kong:"help=${JSONOutputHelp}"`
+	Packages      string        `kong:"default='./...',help=${PackagesHelp}"`
+	ResultsDir    string        `kong:"type=dir,default=${ResultsDirDefault},help=${ResultsDirHelp}"`
+	BenchstatOpts benchstatOpts `kong:"embed"`
 }
 
 func main() {
@@ -109,7 +109,7 @@ func main() {
 	})
 	kctx.FatalIfErrorf(err)
 	if result.HasChangeType(internal.DegradingChange) {
-		os.Exit(cli.DegradationExit)
+		os.Exit(cli.OnDegrade)
 	}
 }
 
